e6006179d47a8423f37ad64468972fe521b49133
angelmenesest123 <angelmenesest123@gmail.com>
2024-05-22 02:23:30 +0000

    feat(card): added properties to dynamically handle card behavior

--- lib/components/card_component.dart
+++ lib/components/card_component.dart
@@ -1,45 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/utils/card_type.dart';
+import 'package:myapp/utils/list_element.dart';
 
 class CardComponent extends StatelessWidget {
   final String title;
   final String assetName;
   final String routeName;
-
+  final CardType cardType;
+  final List<LearningItem>? learningItems;
 
   const CardComponent(
-    {
-      super.key,
+      {super.key,
       required this.title,
       required this.assetName,
-      required this.routeName
-    });
+      required this.routeName,
+      required this.cardType,
+      this.learningItems});
 
   @override
   Widget build(BuildContext context) {
-
     final size = MediaQuery.of(context).size;
+    final width = (cardType.name == CardType.fullCard.name)
+        ? (size.width - 50)
+        : ((size.width - 50) / 2);
+    final height = size.height;
 
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(16.0),
       child: Column(
         children: [
           GestureDetector(
             onTap: () {
-              Navigator.pushNamed(context, routeName);
+              if (learningItems == null) {
+                Navigator.pushNamed(context, routeName);
+              } else {
+                Navigator.pushNamed(
+                    context, 
+                    routeName,
+                    arguments: {
+                      'title': title,
+                      'learningItems': learningItems,
+                    });
+              }
             },
             child: Container(
               decoration: BoxDecoration(
                 color: const Color.fromRGBO(97, 137, 255, 1),
-                borderRadius: BorderRadius.circular(8),
+                borderRadius: BorderRadius.circular(16),
               ),
-              width: size.width - 50,
-              height: size.height * 0.2,
+              width: width,
+              height: height * 0.25,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Image.asset(
                     assetName,
-                    height: size.height * 0.13,
+                    height: height * 0.18,
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
@@ -47,10 +63,7 @@ class CardComponent extends StatelessWidget {
                     padding: const EdgeInsets.all(8),
                     child: Text(
                       title,
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 20),
+                      style: Theme.of(context).textTheme.headlineLarge
                     ),
                   )
                 ],
