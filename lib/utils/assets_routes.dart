f1582183ae3b996328998125dca51d0b9cb745de
angelmenesest123 <angelmenesest123@gmail.com>
2024-05-23 02:12:33 +0000

    feat(assets): added assets route as a static to facilitate access through the app

---
+++ lib/utils/assets_routes.dart
@@ -0,0 +1,52 @@
+class ModuleAsset {
+  // General modules banners
+  static const learningBanner = 'assets/images/banner-aprender.png';
+  static const practiceBanner = 'assets/images/banner-aprender.png';
+  static const dictionaryBanner = 'assets/images/diccionario.png';
+  static const contactBanner = 'assets/images/contacto.png';
+}
+
+class LearningModuleAsset {
+  // Learning modules banners
+  static const basicPhrasesBanner = 'assets/images/frases-basicas.png';
+  static const alphabetBanner = 'assets/images/abecedario.png';
+  static const numbersBanner = 'assets/images/numeros.png';
+  static const familyBanner = 'assets/images/familia.png';
+  static const weekBanner = 'assets/images/semana.png';
+}
+
+class BasicPhrasesAssets {
+  static const hello = 'assets/images/hello.png';
+  static const goodMorning = 'assets/images/good-morning.png';
+  //TO-DO
+}
+
+class AlphabetAssets {
+  static const letterA = 'assets/images/letra-a.png';
+  static const letterB = 'assets/images/letra-b.png';
+  static const letterC = 'assets/images/letra-c.png';
+  static const letterD = 'assets/images/letra-d.png';
+  static const letterE = 'assets/images/letra-e.png';
+  static const letterF = 'assets/images/letra-f.png';
+  static const letterG = 'assets/images/letra-g.png';
+  static const letterH = 'assets/images/letra-h.png';
+  static const letterI = 'assets/images/letra-i.png';
+  static const letterJ = 'assets/images/letra-j.png';
+  static const letterK = 'assets/images/letra-k.png';
+  static const letterL = 'assets/images/letra-l.png';
+  static const letterM = 'assets/images/letra-m.png';
+  static const letterN = 'assets/images/letra-n.png';
+  static const letterO = 'assets/images/letra-o.png';
+  static const letterP = 'assets/images/letra-p.png';
+  static const letterQ = 'assets/images/letra-q.png';
+  static const letterR = 'assets/images/letra-r.png';
+  static const letterS = 'assets/images/letra-s.png';
+  static const letterT = 'assets/images/letra-t.png';
+  static const letterU = 'assets/images/letra-u.png';
+  static const letterV = 'assets/images/letra-v.png';
+  static const letterW = 'assets/images/letra-w.png';
+  static const letterX = 'assets/images/letra-x.png';
+  static const letterY = 'assets/images/letra-y.png';
+  static const letterZ = 'assets/images/letra-z.png';
+}
+
