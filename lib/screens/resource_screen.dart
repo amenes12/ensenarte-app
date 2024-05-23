22be30da0212450b36b243841a5b54a29b90e2af
angelmenesest123 <angelmenesest123@gmail.com>
2024-05-23 02:13:41 +0000

    feat(learning): added FAB to support cam validation for learning resources

--- lib/screens/resource_screen.dart
+++ lib/screens/resource_screen.dart
@@ -18,10 +18,25 @@ class ResourceScreen extends StatelessWidget {
         child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
           Image.asset(
             resourceToLoad!,
-            height: 280.0,
-            width: 160.0,
+            height: 360.0,
+            width: 180.0,
           )
         ]),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          // to-do: navigate to the camera
+        },
+        label: const Text(
+          'Tu turno',
+          style: TextStyle(
+            color: Colors.white, 
+            fontSize: 20.0,
+          ),
+        ),
+        foregroundColor: Colors.white,
+        backgroundColor: const Color.fromRGBO(97, 137, 255, 1),
+        icon: const Icon(Icons.camera_alt_outlined),
       ),
     );
   }
