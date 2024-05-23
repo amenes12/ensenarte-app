90d61d26c4111491db50dad07c78380dfcf84686
angelmenesest123 <angelmenesest123@gmail.com>
2024-05-23 23:43:01 +0000

    refactor(assets): updated references to assets routes

--- lib/utils/assets_routes.dart
+++ lib/utils/assets_routes.dart
@@ -1,52 +1,80 @@
-class ModuleAsset {
+class BannerAsset {
   // General modules banners
-  static const learningBanner = 'assets/images/banner-aprender.png';
-  static const practiceBanner = 'assets/images/banner-aprender.png';
-  static const dictionaryBanner = 'assets/images/diccionario.png';
-  static const contactBanner = 'assets/images/contacto.png';
+  static const learn = 'assets/images/banners/aprender.png';
+  static const practice = 'assets/images/banners/aprender.png';
+  static const dictionary = 'assets/images/banners/diccionario.png';
+  static const contact = 'assets/images/banners/contacto.png';
 }
 
 class LearningModuleAsset {
   // Learning modules banners
-  static const basicPhrasesBanner = 'assets/images/frases-basicas.png';
-  static const alphabetBanner = 'assets/images/abecedario.png';
-  static const numbersBanner = 'assets/images/numeros.png';
-  static const familyBanner = 'assets/images/familia.png';
-  static const weekBanner = 'assets/images/semana.png';
+  static const basicPhrases = 'assets/images/modulos/frases-basicas.png';
+  static const alphabet = 'assets/images/modulos/abecedario.png';
+  static const numbers = 'assets/images/modulos/numeros.png';
+  static const family = 'assets/images/modulos/familia.png';
+  static const week = 'assets/images/modulos/semana.png';
 }
 
-class BasicPhrasesAssets {
-  static const hello = 'assets/images/hello.png';
-  static const goodMorning = 'assets/images/good-morning.png';
-  //TO-DO
+class BasicPhraseAsset {
+  static const hello = 'assets/images/frases-basicas/hola.png';
+  static const goodMorning = 'assets/images/frases-basicas/buenos-dias.png';
+  static const goodNight = 'assets/images/frases-basicas/buenas-noches.png';
+  static const thankYou = 'assets/images/frases-basicas/gracias.png';
+  static const please = 'assets/images/frases-basicas/por-favor.png';
+  static const loveYou = 'assets/images/frases-basicas/te-quiero.png';
+  static const iDontKnow = 'assets/images/frases-basicas/no-se.png';
+  static const whatDoesItMean = 'assets/images/frases-basicas/que-significa.png';
 }
 
-class AlphabetAssets {
-  static const letterA = 'assets/images/letra-a.png';
-  static const letterB = 'assets/images/letra-b.png';
-  static const letterC = 'assets/images/letra-c.png';
-  static const letterD = 'assets/images/letra-d.png';
-  static const letterE = 'assets/images/letra-e.png';
-  static const letterF = 'assets/images/letra-f.png';
-  static const letterG = 'assets/images/letra-g.png';
-  static const letterH = 'assets/images/letra-h.png';
-  static const letterI = 'assets/images/letra-i.png';
-  static const letterJ = 'assets/images/letra-j.png';
-  static const letterK = 'assets/images/letra-k.png';
-  static const letterL = 'assets/images/letra-l.png';
-  static const letterM = 'assets/images/letra-m.png';
-  static const letterN = 'assets/images/letra-n.png';
-  static const letterO = 'assets/images/letra-o.png';
-  static const letterP = 'assets/images/letra-p.png';
-  static const letterQ = 'assets/images/letra-q.png';
-  static const letterR = 'assets/images/letra-r.png';
-  static const letterS = 'assets/images/letra-s.png';
-  static const letterT = 'assets/images/letra-t.png';
-  static const letterU = 'assets/images/letra-u.png';
-  static const letterV = 'assets/images/letra-v.png';
-  static const letterW = 'assets/images/letra-w.png';
-  static const letterX = 'assets/images/letra-x.png';
-  static const letterY = 'assets/images/letra-y.png';
-  static const letterZ = 'assets/images/letra-z.png';
+class AlphabetAsset {
+  static const letterA = 'assets/images/abecedario/letra-a.png';
+  static const letterB = 'assets/images/abecedario/letra-b.png';
+  static const letterC = 'assets/images/abecedario/letra-c.png';
+  static const letterD = 'assets/images/abecedario/letra-d.png';
+  static const letterE = 'assets/images/abecedario/letra-e.png';
+  static const letterF = 'assets/images/abecedario/letra-f.png';
+  static const letterG = 'assets/images/abecedario/letra-g.png';
+  static const letterH = 'assets/images/abecedario/letra-h.png';
+  static const letterI = 'assets/images/abecedario/letra-i.png';
+  static const letterJ = 'assets/images/abecedario/letra-j.png';
+  static const letterK = 'assets/images/abecedario/letra-k.png';
+  static const letterL = 'assets/images/abecedario/letra-l.png';
+  static const letterM = 'assets/images/abecedario/letra-m.png';
+  static const letterN = 'assets/images/abecedario/letra-n.png';
+  static const letterO = 'assets/images/abecedario/letra-o.png';
+  static const letterP = 'assets/images/abecedario/letra-p.png';
+  static const letterQ = 'assets/images/abecedario/letra-q.png';
+  static const letterR = 'assets/images/abecedario/letra-r.png';
+  static const letterS = 'assets/images/abecedario/letra-s.png';
+  static const letterT = 'assets/images/abecedario/letra-t.png';
+  static const letterU = 'assets/images/abecedario/letra-u.png';
+  static const letterV = 'assets/images/abecedario/letra-v.png';
+  static const letterW = 'assets/images/abecedario/letra-w.png';
+  static const letterX = 'assets/images/abecedario/letra-x.png';
+  static const letterY = 'assets/images/abecedario/letra-y.png';
+  static const letterZ = 'assets/images/abecedario/letra-z.png';
 }
 
+class NumberAsset {
+  static const numberOne = 'assets/images/numeros/numero-uno.png';
+}
+
+class FamilyAsset {
+  static const mom = 'assets/images/familia/mama.png';
+  static const dad = 'assets/images/familia/papa.png';
+  static const son = 'assets/images/familia/hijo.png';
+  static const daughter = 'assets/images/familia/hija.png';
+  static const sibling = 'assets/images/familia/hermanos.png';
+  static const grandMa = 'assets/images/familia/abuela.png';
+  static const grandPa = 'assets/images/familia/abuelo.png';
+}
+
+class WeekAsset {
+  static const monday = 'assets/images/semana/lunes.png';
+  static const tuesday = 'assets/images/semana/martes.png';
+  static const wednesday = 'assets/images/semana/miercoles.png';
+  static const thursday = 'assets/images/semana/jueves.png';
+  static const friday = 'assets/images/semana/viernes.png';
+  static const saturday = 'assets/images/semana/sabado.png';
+  static const sunday = 'assets/images/semana/domingo.png';
+}
