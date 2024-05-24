e05b3d43316e78202f3456844f61c7c24d217cd5
angelmenesest123 <angelmenesest123@gmail.com>
2024-05-24 03:02:49 +0000

    feat(style): added general styling to app

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,21 @@ class MyApp extends StatelessWidget {
       routes: AppRouting.getRoutes(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 250, 250, 250),
         primarySwatch: Colors.blue,
         appBarTheme: const AppBarTheme(
+          titleTextStyle:  TextStyle(
+            color: Colors.white,
+            fontSize: 24.0,
+            fontWeight: FontWeight.w600,
+          ),
           color: Color.fromRGBO(97, 137, 255, 1),
           elevation: 2,
+          iconTheme: IconThemeData(
+            color: Colors.white,
+          ),
         ),
+        canvasColor: Colors.green,
         textTheme: const TextTheme(
           headlineLarge: TextStyle(
             fontSize: 20,
