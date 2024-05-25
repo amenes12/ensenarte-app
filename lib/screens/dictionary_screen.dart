94a905981a86178663e3472e5b6b9a6b0ecb2cf2
Angel Meneses <angelmenesest123@gmail.com>
2024-05-24 21:32:22 -0500

    refactor(learn): updated references to new category and learning items path

--- lib/screens/dictionary_screen.dart
+++ lib/screens/dictionary_screen.dart
@@ -1,7 +1,7 @@
 import 'package:ensenarte/components/dictionary_search.dart';
 import 'package:flutter/material.dart';
 import 'package:ensenarte/utils/assets_routes.dart';
-import 'package:ensenarte/utils/list_element.dart';
+import 'package:ensenarte/models/learning_item.dart';
 
 class DictionaryScreen extends StatelessWidget {
   final String title;
@@ -11,131 +11,157 @@ class DictionaryScreen extends StatelessWidget {
       AlphabetAsset.letterA,
       'Abeja',
       AlphabetAsset.letterA,
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Anillo',
       AlphabetAsset.letterA,
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Avión',
       AlphabetAsset.letterA,
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Bicicleta',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Burro',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Casa',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Carro',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Dados',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Duende',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Elefante',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Enano',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Familia',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Fuente',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Gato',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Girasol',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Huevo',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Imán',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Juegos',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Koala',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Limón',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Mesa',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Nuez',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Olla',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Pato',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Queso',
       '',
+      Category.generalWords,
     ),
     LearningItem(
       AlphabetAsset.letterA,
       'Ratón',
       '',
+      Category.generalWords,
     ),
   ];
 
