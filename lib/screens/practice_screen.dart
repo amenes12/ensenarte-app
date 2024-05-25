06a72a1da8fe0ecc2efc79b1b2418692ad8844f0
Angel Meneses <angelmenesest123@gmail.com>
2024-05-24 21:34:32 -0500

    feat(quiz): added quiz component to its screen

--- lib/screens/practice_screen.dart
+++ lib/screens/practice_screen.dart
@@ -1,9 +1,9 @@
+import 'package:ensenarte/components/quiz_component.dart';
 import 'package:flutter/material.dart';
 
 class PracticeScreen extends StatelessWidget {
-  final String title; 
+  final String title;
   const PracticeScreen({super.key, required this.title});
-
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +11,7 @@ class PracticeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-        child: Text('This is my $title screen'),
-      ),
+      body: const QuizComponent(),
     );
   }
 }
