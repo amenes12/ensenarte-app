4a313019345b7119567c503af96b116bb1c80e58
Angel Meneses <angelmenesest123@gmail.com>
2024-05-24 21:31:29 -0500

    feat(learn): added learning category model

---
+++ lib/models/learning_category.dart
@@ -0,0 +1,13 @@
+import 'package:ensenarte/models/learning_item.dart';
+
+class LearningCategory {
+  String title;
+  String assetName;
+  Category category;
+
+  LearningCategory(
+    this.title,
+    this.assetName,
+    this.category,
+  );
+}
