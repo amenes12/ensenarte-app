cb4dae1e3b0c913a71c8b74d949520a3e925ccfa
Angel Meneses <angelmenesest123@gmail.com>
2024-05-28 23:09:04 -0500

    feat(auth): created custom input text component

---
+++ lib/components/input_component.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class InputComponent extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final IconData icon;
+  final bool isPassword;
+
+  const InputComponent({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.icon,
+    this.isPassword = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 24.0,
+        vertical: 8.0,
+      ),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: const TextStyle(
+            color: Color.fromARGB(97, 0, 0, 0),
+            fontSize: 16.0,
+          ),
+          prefixIcon: Icon(
+            icon,
+            color: const Color.fromARGB(97, 0, 0, 0),
+          ),
+          border: InputBorder.none,
+          filled: true,
+          fillColor: const Color.fromARGB(255, 211, 221, 248),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              width: 1.0,
+              color: Color.fromARGB(255, 143, 171, 255),
+            ),
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+        obscureText: isPassword,
+      ),
+    );
+  }
+}
