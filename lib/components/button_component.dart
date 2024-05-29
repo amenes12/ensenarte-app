c9ca71774ffb82fe1c27a58939b37a124811bb05
Angel Meneses <angelmenesest123@gmail.com>
2024-05-28 23:09:17 -0500

    feat(auth): created custom button component

---
+++ lib/components/button_component.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ButtonComponent extends StatelessWidget {
+  final VoidCallback onTap;
+  final String text;
+  final Color color;
+
+  const ButtonComponent({
+    super.key,
+    required this.onTap,
+    required this.text,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+        child: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(
+            vertical: 12.0,
+          ),
+          decoration: ShapeDecoration(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(36.0),
+            ),
+            color: color,
+          ),
+          child: Text(
+            text,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
